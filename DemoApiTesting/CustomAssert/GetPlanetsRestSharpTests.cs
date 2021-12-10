66aa55ca419323246147665c051199ba0dc31fc1
alfabettagamak <alisa.skrynko@gmail.com>
2021-12-10 20:48:16 +0300

    new tests

---
+++ DemoApiTesting/CustomAssert/GetPlanetsRestSharpTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace DemoApiTesting.CustomAssert
+{
+    public class GetPetTests
+    {
+        private const string Host = "https://swapi.dev/api";
+        private const string Api = "/planets";
+        
+        [Test]
+        public void CheckGetPetByIdTesting()
+        {
+
+        }
+    }
+}
